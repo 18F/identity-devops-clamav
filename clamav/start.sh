222e0e154ec4cf6d4defa17d29728826d6280ea4
timothy-spencer <timothy.spencer@gsa.gov>
2020-05-06 16:23:19 -0700

    still logging stuff we shouldn't be

--- clamav/start.sh
+++ clamav/start.sh
@@ -19,7 +19,7 @@ sysctl fs.inotify.max_user_watches=524288
 # watch non-docker system
 watchnondocker() {
 	inotifywait -e close_write -e create --format %w%f -r -m --exclude '\/host-fs\/var\/lib\/docker\/|\/host-fs\/proc\/|\/host-fs\/sys\/|\/host-fs\/dev\/' /host-fs | while read line ; do
-		clamdscan --no-summary --fdpass --stdout --infected "$line"
+		clamdscan --no-summary --fdpass --stdout --infected "$line" | grep -v ': OK'
 	done
 }
 watchnondocker &
@@ -28,7 +28,7 @@ watchnondocker &
 # exclude some stuff that we know is fine (just elasticsearch stuff for now)
 watchdocker() {
 	inotifywait -e close_write -e create --format %w%f -r -m --exclude '/host-fs/var/lib/kubelet/plugins/kubernetes.io/csi/pv/.*/globalmount/nodes' "$1" | while read line ; do
-		clamdscan --no-summary --fdpass --stdout --infected "$line"
+		clamdscan --no-summary --fdpass --stdout --infected "$line" | grep -v ': OK'
 	done
 }
 while true ; do
